2f4aeaf0e430991b8f6d257f02f2808dba4df77c
Nicorici <nickorici@yahoo.com>
2019-02-11 13:55:51 +0200

    Added Product-Feature Project

---
+++ LinqTraining/Product-Feature.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+namespace LinqTraining
+{
+    public class Product_Feature
+    {
+        public class Product
+        {
+            public string Name { get; set; }
+            public ICollection<Feature> Features { get; set; }
+        }
+
+        public class Feature
+        {
+            public int Id { get; set; }
+        }
+
+        static void Main11(string[] args)
+        {
+            var products = new List<Product>
+            {
+               {new Product { Name = "Laptop", Features = new List<Feature> { new Feature { Id = 11 }, new Feature { Id = 22 }, new Feature { Id = 33 }, new Feature { Id = 44 } } } },
+               {new Product { Name = "Mousse", Features = new List<Feature> { new Feature { Id = 22 }, new Feature { Id = 33 }, new Feature { Id = 11 }, new Feature { Id = 12 } } } },
+               {new Product { Name = "Keyboard", Features = new List<Feature> { new Feature { Id = 12 }, new Feature { Id = 12 }, new Feature { Id = 12 }, new Feature { Id = 12 } } } },
+               {new Product { Name = "Monitor", Features = new List<Feature> { new Feature { Id = 11 }, new Feature { Id = 22 }, new Feature { Id = 33 }, new Feature { Id = 44} } } },
+               {new Product { Name = "Webcam", Features = new List<Feature> { new Feature { Id = 22}, new Feature { Id = 1}, new Feature { Id = 1 }, new Feature { Id = 1 } } } },
+               {new Product { Name = "Iphone", Features = new List<Feature> { new Feature { Id = 12 }, new Feature { Id = 12 }, new Feature { Id = 12 }, new Feature { Id = 12 } } } },
+               {new Product { Name = "Headset", Features = new List<Feature> { new Feature { Id = 12 }, new Feature { Id = 12 }, new Feature { Id = 22 }, new Feature { Id = 12 } } } },
+               {new Product { Name = "Joystick", Features = new List<Feature> { new Feature { Id = 12 }, new Feature { Id = 44 }, new Feature { Id = 12 }, new Feature { Id = 12 } } } },
+               {new Product { Name = "MousePad", Features = new List<Feature> { new Feature { Id = 12 }, new Feature { Id = 12 }, new Feature { Id = 12 }, new Feature { Id = 12 } } } }
+            };
+
+            var features = new List<Feature>
+            {
+                {new Feature{ Id=11}},
+                {new Feature{ Id=22}},
+                {new Feature{ Id=33}},
+                {new Feature{ Id=44}},
+
+            };
+
+            var firstQuery = products.Where(product => product.Features.Any(p => features.Any(f => f.Id == p.Id)));
+           
+
+            var secondQuery =
+                from product in products
+                where product.Features.All(p => features.Any(f => f.Id == p.Id))
+                select product;
+
+            var thirdQuery =
+                from product in products
+                where product.Features.All(p => features.All(f => f.Id != p.Id))
+                select product;
+
+        }
+    }
+}
